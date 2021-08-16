ce16c027a55976c741df996d02db65e185264499
김대현 <ungk456@naver.com>
2021-08-16 22:23:43 +0900

    ADD : 주문수정 완료

---
+++ client-reactApollo/src/components/modify/index.js
@@ -0,0 +1,154 @@
+import classNames from 'classnames';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import style from './Modify.module.css';
+import { Link, useHistory } from 'react-router-dom';
+import { useQuery, useMutation, gql } from '@apollo/client';
+
+
+/***********************************
+ * graphQL start
+ ***********************************/
+const EDIT_ORDER = gql`
+    mutation EditOrder($id: ID!, $input: PostOrderInput!){
+        editOrder(id:$id, input: $input){
+            id
+            user_id,
+            color,
+            title,
+            hashtag1,
+            hashtag2,
+            hashtag3,
+            name,
+            phone,
+            addr,
+            price,
+            is_pay
+        }
+    },
+`;
+/***********************************
+* graphQL end
+***********************************/
+
+
+// refetch는 orderlist 에서 해주고 있기 때문에, 변경 값이 화면에 갱신되므로 닫음.
+// let refetchOrder; // refetch variables
+
+function Modify({ order, setIsShowModify }) {
+    const cx = classNames.bind(style); // classNames
+
+    /***********************************
+     * states
+     ***********************************/
+    const [colorChecked, setColorChecked] = useState(order.color);
+    const [inputs, setInputs] = useState({
+        user_id:order.user_id,
+        color:order.color,
+        title:order.title,
+        hashtag1:order.hashtag1,
+        hashtag2:order.hashtag2,
+        hashtag3:order.hashtag3,
+        name:order.name,
+        phone:order.phone,
+        addr:order.addr,
+        price:order.price,
+        is_pay:order.is_pay
+    });
+
+    /***********************************
+     * apollo client
+     ***********************************/
+    // const { data, refetch } = useQuery(GET_ORDERS);
+    const [editOrder] = useMutation( EDIT_ORDER, { onCompleted: editOrderCompleted })
+    
+    function execEditOrder(){
+        editOrder({
+            variables:{
+                id:order.id, 
+                input:inputs
+            }
+        })
+    }
+
+    function editOrderCompleted(){
+        alert('상품을 수정했어요')
+        setIsShowModify(false)
+    }
+
+    /***********************************
+     * functions
+     ***********************************/
+    const changeInputs = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const clickSubmit = () => {
+        if( window.confirm('이 정보로 수정하시겠어요 ?') ){
+            execEditOrder();
+            // refetchOrder = refetch;
+            // refetchOrder();
+        }
+    }
+
+
+    /***********************************
+     * components
+     ***********************************/
+    const InputColorCode = useCallback(({ color }) =>
+        <label>
+            <input 
+                type="radio" 
+                name="color" 
+                value={color} 
+                onChange={(e) => {
+                    setColorChecked(color);
+                    changeInputs(e);
+                }}
+            />
+            <span className={style.ColorCode}></span>
+        </label>
+    ,[]);
+
+    const Inputbox = useCallback(({ title, value, name, type }) => 
+        <div className={style.Inputbox}>
+            <p>{title}</p>
+            <input 
+                type={type} 
+                value={value} 
+                name={name}
+                onChange={changeInputs} 
+                className={style.InputStyle} 
+            />
+            <button onClick={clickSubmit} style={{backgroundColor:colorChecked}}>수정</button>
+        </div>
+    , [colorChecked]);
+
+
+    return (
+        <div className={style.Modify}>
+            <h3>상품 수정</h3>
+            <div className={style.Inputbox}>
+                <p>상품 색상</p>
+                <div className={style.Colorbox}>
+                    <InputColorCode color={"red"} />
+                    <InputColorCode color={"blue"} />
+                    <InputColorCode color={"green"} />
+                    <InputColorCode color={"violet"} />
+                </div>
+            </div>
+            <Inputbox title={"상품 이름"} value={inputs.title} name={'title'} type={"text"} />
+            <Inputbox title={"상품 해시태그1"} value={inputs.hashtag1} name={'hashtag1'} type={"text"} />
+            <Inputbox title={"상품 해시태그2"} value={inputs.hashtag2} name={'hashtag2'} type={"text"} />
+            <Inputbox title={"상품 해시태그3"} value={inputs.hashtag3} name={'hashtag3'} type={"text"} />
+            <Inputbox title={"주문자 이름"} value={inputs.name} name={'name'} type={"text"} />
+            <Inputbox title={"주문자 번호"} value={inputs.phone} name={'phone'} type={"number"} />
+            <Inputbox title={"주문자 주소"} value={inputs.addr} name={'addr'} type={"text"} />
+            <button className={cx('btn_common', 'cancel', 'w100')} onClick={() => setIsShowModify(false)}>수정 취소</button>
+        </div>
+    );
+}
+
+export default Modify;
--- client-reactApollo/src/pages/orderList/index.js
+++ client-reactApollo/src/pages/orderList/index.js
@@ -5,6 +5,7 @@ import { Link, useHistory } from 'react-router-dom';
 import { useQuery, useMutation, gql } from '@apollo/client';
 import { onError } from "apollo-link-error";
 import { colorcode } from '../../util/colorcode'
+import Modify from '../../components/modify';
 
 /***********************************
  * graphQL start
@@ -46,12 +47,18 @@ function OrderList() {
     const history = useHistory(); // router
     const user = localStorage.getItem('user') // for user_id
 
+    /***********************************
+     * states
+     ***********************************/
+    const [orderForModify, setOrderForModify] = useState('');
+    const [isShowModify, setIsShowModify] = useState(false);
     
     /***********************************
      * functions
      ***********************************/
-    const clickEditOrder = (id) => {
-        console.log("🚀 ~ file: index.js ~ line 61 ~ clickEditOrder ~ id", id)
+    const clickEditOrder = (order) => {
+        setOrderForModify(order);
+        setIsShowModify(true);
     }
 
     const clickDeleteOrder = (id) => deleteOrder({ variables:{id: id} });
@@ -62,10 +69,11 @@ function OrderList() {
      ***********************************/
     const { data } = useQuery(GET_ORDERS);
 
+
     const [deleteOrder] = useMutation(DELETE_ORDER,
         {
             onCompleted: deleteOrderCompleted,
-            refetchQueries: [
+            refetchQueries: [ // 삭제 후 화면 갱신
                 { query: GET_ORDERS }
             ]
         },
@@ -73,78 +81,83 @@ function OrderList() {
 
     const user_data = data?.orders.filter(order => order.user_id === JSON.parse(user).user.id );
 
-    
     return (
-        <div className={style.OrderList}>
-            <h2>님의 주문내역</h2>
+        <>
+            <div className={style.OrderList}>
+                <h2>{user_data != undefined && user_data[0].name}님의 주문내역</h2>
 
-            <ul className={style.List}>
-                {
-                    ( !user_data || user_data.length===0
-                    ) ? ( <li>주문 내역이 없습니다.</li>
-                    ) : ( 
-                        user_data.map((order, i) => 
-                            <li key={i}>
-                                <div className={style.Productbox}>
-                                    <div className={cx('Item', 'Item1')}>
-                                        <div className={style.InnerText}>
-                                            <div className={style.ColorNum}>{colorcode(order.color)}</div>
-                                            <div className={style.ItemName}>SHAMPOO</div>
-                                            <div className={style.CodeName}>{order.title !== '' ? order.title : 'BEAUTY'}</div>
-                                            <div className={style.Hashtag}>
-                                                {`#${order.hashtag1} `}
-                                                {`#${order.hashtag2} `}
-                                                {`#${order.hashtag3} `}
+                <ul className={style.List}>
+                    {
+                        ( !user_data || user_data.length===0
+                        ) ? ( <li>주문 내역이 없습니다.</li>
+                        ) : ( 
+                            user_data.map((order, i) => 
+                                <li key={i}>
+                                    <div className={style.Productbox}>
+                                        <div className={cx('Item', 'Item1')}>
+                                            <div className={style.InnerText}>
+                                                <div className={style.ColorNum}>{colorcode(order.color)}</div>
+                                                <div className={style.ItemName}>SHAMPOO</div>
+                                                <div className={style.CodeName}>{order.title !== '' ? order.title : 'BEAUTY'}</div>
+                                                <div className={style.Hashtag}>
+                                                    {`#${order.hashtag1} `}
+                                                    {`#${order.hashtag2} `}
+                                                    {`#${order.hashtag3} `}
+                                                </div>
+                                                <div className={style.Size}>300ml / 10.14 fl. oz.</div>
                                             </div>
-                                            <div className={style.Size}>300ml / 10.14 fl. oz.</div>
+                                            <img src={`/images/goods/goods_${order.color}.png`} alt="상품이미지1" />
                                         </div>
-                                        <img src={`/images/goods/goods_${order.color}.png`} alt="상품이미지1" />
-                                    </div>
 
-                                    <div className={cx('Item', 'Item2')}>
-                                        <div className={style.InnerText}>
-                                            <div className={style.ColorNum}>{colorcode(order.color)}</div>
-                                            <div className={style.ItemName}>CONDITIONER</div>
-                                            <div className={style.CodeName}>{order.title !== '' ? order.title : 'BEAUTY'}</div>
-                                            <div className={style.Hashtag}>
-                                                {`#${order.hashtag1} `}
-                                                {`#${order.hashtag2} `}
-                                                {`#${order.hashtag3} `}
+                                        <div className={cx('Item', 'Item2')}>
+                                            <div className={style.InnerText}>
+                                                <div className={style.ColorNum}>{colorcode(order.color)}</div>
+                                                <div className={style.ItemName}>CONDITIONER</div>
+                                                <div className={style.CodeName}>{order.title !== '' ? order.title : 'BEAUTY'}</div>
+                                                <div className={style.Hashtag}>
+                                                    {`#${order.hashtag1} `}
+                                                    {`#${order.hashtag2} `}
+                                                    {`#${order.hashtag3} `}
+                                                </div>
+                                                <div className={style.Size}>300ml / 10.14 fl. oz.</div>
                                             </div>
-                                            <div className={style.Size}>300ml / 10.14 fl. oz.</div>
+                                            <img src={`/images/goods/goods_${order.color}.png`} alt="상품이미지2" />
                                         </div>
-                                        <img src={`/images/goods/goods_${order.color}.png`} alt="상품이미지2" />
                                     </div>
-                                </div>
 
-                                <div className={style.Userbox}>
-                                    <p>
-                                        <span>이름</span>
-                                        <span>{order.name}</span>
-                                    </p>
-                                    <p>
-                                        <span>전화번호</span>
-                                        <span>{order.phone}</span>
-                                    </p>
-                                    <p>
-                                        <span>주소</span>
-                                        <span>{order.addr}</span>
-                                    </p>
-                                    <p>
-                                        <span>결제금액</span>
-                                        <span>{order.price}원</span>
-                                    </p>
-                                    <div className={style.Btnbox}>
-                                        <button className="btn_common" onClick={() => clickEditOrder(order.id)} style={{backgroundColor:order.color}}>주문수정</button>
-                                        <button className={cx('btn_common', 'cancel')} onClick={() => clickDeleteOrder(order.id)}>주문취소</button>
+                                    <div className={style.Userbox}>
+                                        <p>
+                                            <span>이름</span>
+                                            <span>{order.name}</span>
+                                        </p>
+                                        <p>
+                                            <span>전화번호</span>
+                                            <span>{order.phone}</span>
+                                        </p>
+                                        <p>
+                                            <span>주소</span>
+                                            <span>{order.addr}</span>
+                                        </p>
+                                        <p>
+                                            <span>결제금액</span>
+                                            <span>{order.price}원</span>
+                                        </p>
+                                        <div className={style.Btnbox}>
+                                            <button className="btn_common" onClick={() => clickEditOrder(order)} style={{backgroundColor:order.color}}>주문수정</button>
+                                            <button className={cx('btn_common', 'cancel')} onClick={() => clickDeleteOrder(order.id)}>주문취소</button>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        )
-                    ) // ) : ( 
-                } 
-            </ul>
-        </div>
+                                </li>
+                            )
+                        ) // ) : ( 
+                    } 
+                </ul>
+            </div>
+            {
+                (isShowModify === true && !orderForModify === false) &&
+                <Modify order={orderForModify} setIsShowModify={setIsShowModify} />
+            }
+        </>
     );
 }
 
